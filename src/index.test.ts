8280e846e1554c38b35a6ca810849519a101dbdf
Bryan Müller <bmuller0975@gmail.com>
2021-02-20 00:30:35 -0600

    tests for emit

--- src/index.test.ts
+++ src/index.test.ts
@@ -25,7 +25,9 @@ describe("Event Emitter", () => {
 
       expect(eventMap.get(testEventSymbol)).toEqual([callback1, callback2]);
     });
+  });
 
+  describe("emit", () => {
     it("should invoke the subscribers when event is emitted", () => {
       const callback1 = jest.fn();
       const callback2 = jest.fn();
@@ -36,6 +38,27 @@ describe("Event Emitter", () => {
       emit(testEventSymbol);
       expect(callback1).toHaveBeenCalledTimes(1);
       expect(callback2).toHaveBeenCalledTimes(1);
+    });
+
+    it("should pass the same payload to each subscriber", () => {
+      const callback1 = jest.fn();
+      const callback2 = jest.fn();
+
+      const payload = Symbol("payload");
+
+      subscribe(testEventSymbol, callback1);
+      subscribe(testEventSymbol, callback2);
+
+      emit(testEventSymbol, payload);
+      expect(callback1).toHaveBeenCalledWith(payload);
+      expect(callback2).toHaveBeenCalledWith(payload);
+    });
+
+    it("should do nothing if no subscribers exist for an event", () => {
+      const callback = jest.fn();
+      subscribe(testEventSymbol, callback);
+      emit(Symbol("ignored:event"), "payload");
+      expect(callback).not.toHaveBeenCalled();
     });
   });
 });
