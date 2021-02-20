512afdebd5e1dfaa1e0ed55d60eb7d736e3445ac
Bryan Müller <bmuller0975@gmail.com>
2021-02-20 13:51:20 -0600

    create unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,44 @@
 export type EventMap = Map<string | Symbol, Array<SubscriberCallback<any>>>;
 export type SubscriberCallback<T> = <T>(payload: T) => void;
-export type Subscriber = <T>(
+export type Subscribe = <T>(
   event: string | Symbol,
   callback: SubscriberCallback<T>
-) => void;
+) => Unsubscriber;
+export type Unsubscribe = <T>(event: string | Symbol, callback: SubscriberCallback<T>) => boolean;
+export type Unsubscriber = () => ReturnType<Unsubscribe>
 export type Emitter = <T>(event: string | Symbol, payload?: T) => void;
 
-const createEventBus = () => {
+export const createEventBus = () => {
+  /**
+   * Tracks the registered events and their subscribers
+   */
   const eventMap: EventMap = new Map();
 
-  const subscribe: Subscriber = (event, callback) => {
-    const subscribers = eventMap.get(event);
-
-    if (subscribers) {
-      eventMap.set(event, [...subscribers, callback]);
-    } else {
-      eventMap.set(event, [callback]);
-    }
+  const subscribe: Subscribe = (event, callback) => {
+    const subscribers = eventMap.get(event) ?? [];
+    eventMap.set(event, [...subscribers, callback]);
+    return () => unsubscribe(event, callback)
   };
 
-  const emit: Emitter = (event, payload) => {
-    const subscribers = eventMap.get(event);
+  const unsubscribe: Unsubscribe = (event, callback) => {
+    const subscribers = eventMap.get(event) ?? []
+    const index = subscribers.indexOf(callback)
+    if (index >= 0) {
+      subscribers.splice(index, 1)
+      eventMap.set(event, subscribers)
+      return true
+    } else {
+      return false
+    }
+  }
 
-    if (!subscribers) return;
+  const emit: Emitter = (event, payload) => {
+    const subscribers = eventMap.get(event) ?? [];
 
     subscribers.forEach((subscriber) => subscriber(payload));
   };
 
-  return { emit, subscribe, eventMap };
+  return { emit, subscribe, unsubscribe, eventMap };
 };
 
 export const { emit, subscribe, eventMap } = createEventBus();
